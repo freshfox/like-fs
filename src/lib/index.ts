2421761fa2405926982b0fe58b7d66343e543118
Dominic Bartl <dominic.bartl.at@gmail.com>
2019-11-16 11:38:09 +0100

    Export classes from index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export * from './filesystem/filesystem';
+export * from './filesystem/local_file_system';
+export * from './filesystem/module';
+export * from './filesystem/online_filesystem';
+export * from './filesystem/tmp_file_system';
+export * from './filesystem/utils';
---
+++ src/test/index.ts
@@ -0,0 +1,1 @@
+export * from './filesystem_test_suite';

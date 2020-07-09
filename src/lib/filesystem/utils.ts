045cd32eaef56dc069951eae1f7043f06306fefd
Dominic Bartl <dominic.bartl.at@gmail.com>
2020-07-09 16:28:27 +0200

    Add 0 dependency download utility function

--- src/lib/filesystem/utils.ts
+++ src/lib/filesystem/utils.ts
@@ -1,5 +1,7 @@
 import {Readable, Writable} from 'stream';
 import * as crypto from 'crypto';
+import {get as getHttps} from 'https';
+import {get as getHttp, IncomingMessage} from "http";
 
 export function awaitWriteFinish(stream: Writable) {
 	return new Promise((resolve, reject) => {
@@ -18,4 +20,32 @@ export function writeToStream(data: any): Readable {
 	s.push(data);
 	s.push(null);
 	return s;
+}
+
+class ResponseError extends Error {
+
+	constructor(readonly response: IncomingMessage) {
+		super(`Request returned status code ${response.statusCode}`);
+	}
+
+}
+
+export async function downloadFileFromUrl(url: string, writeStream: Writable) {
+	const resp = await new Promise<IncomingMessage>((resolve, reject) => {
+
+		let get = getHttp;
+		if (url.startsWith('https')) {
+			get = getHttps
+		}
+
+		get(url, (resp) => {
+			if (resp.statusCode >= 400) {
+				reject(new ResponseError(resp));
+			} else {
+				resolve(resp)
+			}
+		});
+	});
+	resp.pipe(writeStream);
+	return awaitWriteFinish(writeStream)
 }
---
+++ src/test/utils_test.ts
@@ -0,0 +1,31 @@
+import {downloadFileFromUrl, TmpFilesystem} from "../lib";
+import * as should from 'should';
+
+describe('Utils', function () {
+
+	describe('#downloadFileFromUrl()', function () {
+
+		it('should download a file with an https url', async () => {
+
+			const url = 'https://file-examples.com/wp-content/uploads/2017/10/file-sample_150kB.pdf';
+			const fs = new TmpFilesystem({});
+
+			const target =`sample-file-1-${Date.now()}.pdf`;
+			await downloadFileFromUrl(url, fs.createWriteStream(target));
+			await should(fs.exists(target)).resolvedWith(true)
+
+		});
+
+		it('should download a file with an http url', async () => {
+
+			const url = 'http://file-examples.com/wp-content/uploads/2017/10/file-sample_150kB.pdf';
+			const fs = new TmpFilesystem({});
+			const target =`sample-file-2-${Date.now()}.pdf`;
+			await downloadFileFromUrl(url, fs.createWriteStream(target));
+			await should(fs.exists(target)).resolvedWith(true)
+
+		});
+
+	});
+
+})

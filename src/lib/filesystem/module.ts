973e10e8767ffc48135778927f2427b6e64f4bdb
Dominic Bartl <dominic.bartl.at@gmail.com>
2020-09-23 12:55:56 +0200

    Extend ContainerModule instead of instantiating it in constructor

--- src/lib/filesystem/module.ts
+++ src/lib/filesystem/module.ts
@@ -3,12 +3,10 @@ import {Filesystem, IFilesystem} from './filesystem';
 import {ITmpFilesystemConfig, TmpFilesystem, TmpFilesystemConfig} from './tmp_file_system';
 import {ContainerModule, interfaces} from 'inversify';
 
-export class FilesystemModule {
-
-	private module: ContainerModule;
+export class FilesystemModule extends ContainerModule {
 
 	constructor(fs: interfaces.Newable<IFilesystem>, config?: ITmpFilesystemConfig) {
-		this.module = new ContainerModule((bind) => {
+		super((bind) => {
 			bind(TmpFilesystem).toSelf().inSingletonScope();
 			bind(LocalFilesystem).toSelf().inSingletonScope();
 			bind<IFilesystem>(Filesystem).to(fs).inSingletonScope();
@@ -18,8 +16,8 @@ export class FilesystemModule {
 		})
 	}
 
+	/**@deprecated*/
 	static create(fs: interfaces.Newable<IFilesystem>, config?: ITmpFilesystemConfig): ContainerModule {
-		let md = new FilesystemModule(fs, config);
-		return md.module;
+		return new FilesystemModule(fs, config);
 	}
 }

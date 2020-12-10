cdc40772a08961c00cdc58c86b66032f1c59f4b1
Dominic Bartl <dominic.bartl.at@gmail.com>
2020-12-10 17:30:38 +0100

    Make Nest module asynchronously configurable

--- src/lib/module.ts
+++ src/lib/module.ts
@@ -1,27 +1,115 @@
-import {DynamicModule, Provider} from "@nestjs/common";
+import {DynamicModule, FactoryProvider, ModuleMetadata, Provider, Type} from "@nestjs/common";
 import {GCSFilesystem, GCStorage, GCStorageConfig, IGCStorageConfig} from "./gcs_filesystem";
 import {Storage} from "@google-cloud/storage";
 
 export class GCSFilesystemModule {
 
 	static forRoot(config: IGCStorageConfig): DynamicModule {
-		const storage = new Storage(config.storageOptions);
-		const providers: Provider[] = [
-			GCSFilesystem,
-			{
-				provide: GCStorageConfig,
-				useValue: config
-			},
-			{
-				provide: GCStorage,
-				useValue: storage
-			}
-		]
 		return {
 			module: GCSFilesystemModule,
-			providers: providers,
-			exports: providers,
+			providers: [
+				GCSFilesystem,
+				{
+					provide: GCStorageConfig,
+					useValue: config
+				},
+				{
+					provide: GCStorage,
+					useValue: new Storage(config.storageOptions)
+				}
+			],
+			exports: [
+				GCSFilesystem
+			],
 		}
 	}
 
+	static forRootAsync(options: GCSFilesystemModuleAsyncOptions): DynamicModule{
+		return {
+			imports: options.imports || [],
+			module: GCSFilesystemModule,
+			providers: [
+				GCSFilesystem,
+				{
+					provide: GCStorage,
+					useFactory: (config: IGCStorageConfig) => {
+						return new Storage(config.storageOptions);
+					},
+					inject: [GCStorageConfig]
+				},
+				...this.createConnectProviders(options)
+			],
+			exports: [
+				GCSFilesystem
+			],
+		};
+	}
+
+	private static createConnectProviders(
+		options: GCSFilesystemModuleAsyncOptions,
+	): Provider[] {
+		if (options.useExisting || options.useFactory) {
+			return [this.createConnectOptionsProvider(options)];
+		}
+
+		// for useClass
+		return [
+			this.createConnectOptionsProvider(options),
+			{
+				provide: options.useClass,
+				useClass: options.useClass,
+			},
+		];
+	}
+
+	private static createConnectOptionsProvider(
+		options: GCSFilesystemModuleAsyncOptions,
+	): Provider {
+		if (options.useFactory) {
+
+			// for useFactory
+			return {
+				provide: GCStorageConfig,
+				useFactory: options.useFactory,
+				inject: options.inject || [],
+			};
+		}
+
+		// For useExisting...
+		return {
+			provide: GCStorageConfig,
+			useFactory: async (optionsFactory: GCSFilesystemOptionsFactory) =>
+				await optionsFactory.createGCSFilesystemOptions(),
+			inject: [options.useExisting || options.useClass],
+		};
+	}
+}
+
+export interface GCSFilesystemOptionsFactory {
+	createGCSFilesystemOptions(): Promise<IGCStorageConfig> | IGCStorageConfig;
+}
+
+export interface GCSFilesystemModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
+
+	/**
+	 * Existing Provider to be used.
+	 */
+	useExisting?: Type<GCSFilesystemOptionsFactory>;
+
+	/**
+	 * Type (class name) of provider (instance to be registered and injected).
+	 */
+	useClass?: Type<GCSFilesystemOptionsFactory>;
+
+	/**
+	 * Factory function that returns an instance of the provider to be injected.
+	 */
+	useFactory?: (
+		...args: any[]
+	) => Promise<IGCStorageConfig> | IGCStorageConfig;
+
+	/**
+	 * Optional list of providers to be injected into the context of the Factory function.
+	 */
+	inject?: FactoryProvider['inject'];
 }
--- src/test/dependency_incjection_test.ts
+++ src/test/dependency_incjection_test.ts
@@ -1,26 +1,61 @@
-import {Module} from "@nestjs/common";
-import {GCSFilesystem, GCStorage, GCStorageConfig} from "../lib";
+import {Module, Provider} from "@nestjs/common";
+import {GCSFilesystem, GCStorage, GCStorageConfig, IGCStorageConfig} from "../lib";
 import {Test} from "@nestjs/testing";
 import 'should';
 import {Container} from "inversify";
-import {GCSFilesystemModule} from "../lib/module";
+import {GCSFilesystemModule, GCSFilesystemOptionsFactory} from "../lib/module";
 import {Storage} from "@google-cloud/storage";
 
 describe('Dependency Injection', function () {
 
 	describe('NestJS', function () {
 
-		const module = GCSFilesystemModule.forRoot({
-			storageBucket: 'my-test-bucket',
+		describe('Static', function () {
+
+			it('should get a GCS fs instance', async () => {
+
+				const module = GCSFilesystemModule.forRoot({
+					storageBucket: 'my-test-bucket',
+				});
+
+				@Module({
+					imports: [module],
+					exports: [module],
+				})
+				class TestModule {}
+
+				const moduleRef = await Test.createTestingModule({
+					imports: [TestModule],
+					exports: [TestModule]
+				}).compile();
+
+				const fs = moduleRef.get(GCSFilesystem);
+				fs.should.instanceOf(GCSFilesystem);
+
+				const storage = moduleRef.get(GCStorage);
+				storage.should.instanceOf(Storage);
+			});
 		});
 
-		@Module({
-			imports: [module],
-			exports: [module],
-		})
-		class TestModule {}
+		it('should configure GCSFilesystemModule asynchronously via useClass', async () => {
 
-		it('should get a GCS fs instance', async () => {
+			class GCSConfigService implements GCSFilesystemOptionsFactory {
+				createGCSFilesystemOptions(): Promise<IGCStorageConfig> | IGCStorageConfig {
+					return {
+						storageBucket: 'bucket',
+						storageOptions: {}
+					};
+				}
+			}
+
+			@Module({
+				imports: [
+					GCSFilesystemModule.forRootAsync({
+						useClass: GCSConfigService
+					})
+				]
+			})
+			class TestModule {}
 
 			const moduleRef = await Test.createTestingModule({
 				imports: [TestModule],
@@ -32,7 +67,51 @@ describe('Dependency Injection', function () {
 
 			const storage = moduleRef.get(GCStorage);
 			storage.should.instanceOf(Storage);
+
 		});
+
+		it('should configure GCSFilesystemModule asynchronously via useFactory, import and external dependency ', async () => {
+			const GCS_CONFIG = 'GCS_CONFIG';
+			const config: IGCStorageConfig = {
+				storageBucket: 'bucket',
+				storageOptions: {}
+			};
+			const configProviders: Provider[] = [{
+				provide: GCS_CONFIG,
+				useValue: config
+			}];
+			@Module({
+				providers: configProviders,
+				exports: configProviders
+			})
+			class ConfigModule {}
+
+			@Module({
+				imports: [
+					GCSFilesystemModule.forRootAsync({
+						imports: [ConfigModule],
+						useFactory: (config: IGCStorageConfig) => {
+							return config;
+						},
+						inject: [GCS_CONFIG]
+					})
+				]
+			})
+			class TestModule {}
+
+			const moduleRef = await Test.createTestingModule({
+				imports: [TestModule],
+				exports: [TestModule]
+			}).compile();
+
+			const fs = moduleRef.get(GCSFilesystem);
+			fs.should.instanceOf(GCSFilesystem);
+
+			const storage = moduleRef.get(GCStorage);
+			storage.should.instanceOf(Storage);
+
+		});
+
 	});
 
 	describe('Inversify', function () {

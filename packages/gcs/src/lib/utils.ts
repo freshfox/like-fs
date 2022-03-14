0d2d96d39f5ff3a170c16919b0b63f5f0b1ca28c
Dominic Bartl <dominic.bartl.at@gmail.com>
2022-03-14 15:49:56 +0100

    Allow IOnlineFilesystem to be passed to FirebaseUtils

--- packages/gcs/src/lib/utils.ts
+++ packages/gcs/src/lib/utils.ts
@@ -3,11 +3,12 @@ import {IOnlineFilesystem} from "like-fs";
 
 export class FirebaseUtils {
 
-	static createUrl(bucket: string, path: string, token: string){
-		return `https://firebasestorage.googleapis.com/v0/b/${bucket}/o/${encodeURIComponent(path)}?alt=media&token=${token}`
+	static createUrl(bucketOrFs: string | IOnlineFilesystem, path: string, token: string) {
+		let bucketName = typeof bucketOrFs === 'string' ? bucketOrFs : bucketOrFs.getBucketName();
+		return `https://firebasestorage.googleapis.com/v0/b/${bucketName}/o/${encodeURIComponent(path)}?alt=media&token=${token}`
 	}
 
-	static generateTokenAndUrl(bucket: string, path: string) {
+	static generateTokenAndUrl(bucket: string | IOnlineFilesystem, path: string) {
 		const token = uuid();
 		return {
 			token,

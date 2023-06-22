1ea387b35503b4bb21b79ef10308af487689ac25
Dominic Bartl <dominic.bartl.at@gmail.com>
2023-06-22 15:41:05 +0200

    fix(gcs): remove unused module

--- packages/gcs/src/index.ts
+++ packages/gcs/src/index.ts
@@ -1,4 +1,3 @@
-export {CreateWriteStreamOptions, CreateReadStreamOptions} from "@google-cloud/storage";
+export { CreateWriteStreamOptions, CreateReadStreamOptions } from '@google-cloud/storage';
 export * from './gcs_filesystem';
-export * from './module';
 export * from './utils';
